1c859867a21dc66bf4c77992dd7cdd3bf57e8af9
spoe-code <124516124+spoe-code@users.noreply.github.com>
2023-07-25 08:18:46 -0400

    updated footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import WeatherSearch from "./WeatherSearch";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather Search</h1>
+        <WeatherSearch />
       </header>
     </div>
   );
 }
+<footer>
+  <a href="https://github.com/spoe-code/reactweather4">
+    Open-source code on GitHub
+  </a>
+  by Samantha Poe
+</footer>;
 
 export default App;
---
+++ src/WeatherSearch.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    console.log(response.data);
+    setWeather({
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "073c348fbaftb842o6700ee01183a6a5";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city ..."
+        onChange={updateCity}
+      />
+      <input type="Submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {" "}
+        {form}
+        <ul>
+          <li> {Math.round(weather.temperature)}°C </li>
+          <li> {weather.description} </li>
+          <li> {weather.wind} % </li>
+          <li> {weather.humidity} km/h </li>
+          <li>
+            {" "}
+            <img src={weather.icon} alt={weather.description} />{" "}
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
